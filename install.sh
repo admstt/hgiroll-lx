a84f56f3ca661209193552ff34d705b3ec17073b
admstt <tyuipop96@gmail.com>
2020-07-25 22:26:24 +0700

    update

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/usr
+
+sudo su
+rm -rf ~/MonVIP
+mkdir ~/MonVIP
+cd .. && cp -r hgiroll/* ~/MonVIP
+rm -rf hgiroll
+cd ~/MonVIP/server
+apt install proot -y
+apt install nodejs -y
+npm install -no-audit; npm install -g pm2 -no-audit && cd .. && rm -rf install.sh
+python run.py
+cd ~/
+ls
